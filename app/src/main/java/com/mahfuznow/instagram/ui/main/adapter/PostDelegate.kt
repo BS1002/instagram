c6e7764b040c4dfe31620b2e55fff23504ca211b
Md Mahfuzur Rahman <mahfuznow@gmail.com>
2022-02-27 00:18:10 +0600

    Fix Issue: RecyclerView - toggling one checkBox toggles some other checkBoxs

--- app/src/main/java/com/mahfuznow/instagram/ui/main/adapter/PostDelegate.kt
+++ app/src/main/java/com/mahfuznow/instagram/ui/main/adapter/PostDelegate.kt
@@ -1,5 +1,6 @@
 package com.mahfuznow.instagram.ui.main.adapter
 
+import android.util.SparseBooleanArray
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -24,8 +25,41 @@ class PostDelegate @Inject constructor() : AdapterDelegate<ArrayList<Any>>() {
         item as PostsData.Data
         holder as PostViewHolder
 
-        holder.binding.run {
-            post = item //Data binding
+        //Data binding
+        holder.binding.post = item
+
+        holder.onBind(item, position)
+    }
+
+    class PostViewHolder(val binding: ItemPostBinding) : RecyclerView.ViewHolder(binding.root) {
+
+        /**
+         * Recycler View recycled it's views to reduce memory consumption
+         * But that causes duplicate View states (For example if we check an checkbox
+         * on one item, this state will reflect on another item as the previous view is reused)
+         * We can prevent this by storing CheckState of each position in a (int -> bool) map
+         * in OnClickListener and restoring from it to render Proper checkState
+         *
+         * In real case scenario this states should be fetched from data source via DataModel
+         * In that case we can store those states in corresponding data Model
+         */
+        private val favouriteStates = SparseBooleanArray()
+        private val bookmarkStates = SparseBooleanArray()
+        private val followStates = SparseBooleanArray()
+
+        private val context = binding.root.context
+        private val favourite = binding.favourite
+        private val bookmark = binding.bookmark
+        private val follow = binding.follow
+        private val image = binding.image
+
+        fun onBind(item: PostsData.Data, position: Int) {
+
+            //Restore checked state when view is recycled
+            favourite.isChecked = favouriteStates.get(position)
+            bookmark.isChecked = bookmarkStates.get(position)
+            follow.isChecked = followStates.get(position)
+            setFollowText()
 
             image.setOnClickListener(
                 object : DoubleClickListener() {
@@ -37,14 +71,31 @@ class PostDelegate @Inject constructor() : AdapterDelegate<ArrayList<Any>>() {
 
             follow.run {
                 setOnClickListener {
-                    text = if (isChecked)
-                        context.getString(R.string.following)
-                    else
-                        context.getString(R.string.follow)
+                    followStates.put(position, isChecked)
+                    setFollowText()
+                }
+            }
+
+            favourite.run {
+                setOnClickListener {
+                    favouriteStates.put(position, isChecked)
+                }
+            }
+            bookmark.run {
+                setOnClickListener {
+                    bookmarkStates.put(position, isChecked)
+                }
+            }
+        }
+
+        private fun setFollowText() {
+            follow.run {
+                text = if (isChecked) {
+                    context.getString(R.string.following)
+                } else {
+                    context.getString(R.string.follow)
                 }
             }
         }
     }
-
-    class PostViewHolder(val binding: ItemPostBinding) : RecyclerView.ViewHolder(binding.root)
 }
