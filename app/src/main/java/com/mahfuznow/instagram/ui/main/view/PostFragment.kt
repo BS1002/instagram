f555a1e97211342dc0fa2c7b2496524cf6796161
Md Mahfuzur Rahman <mahfuznow@gmail.com>
2022-03-01 19:47:23 +0600

    Removed Deprecated code - Used Activity Results API (from ktx) to Pick image from gallery and from camera

--- app/src/main/java/com/mahfuznow/instagram/ui/main/view/PostFragment.kt
+++ app/src/main/java/com/mahfuznow/instagram/ui/main/view/PostFragment.kt
@@ -1,13 +1,11 @@
 package com.mahfuznow.instagram.ui.main.view
 
-import android.app.Activity
-import android.content.Intent
-import android.graphics.Bitmap
 import android.os.Bundle
-import android.provider.MediaStore
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.activity.result.launch
 import androidx.appcompat.app.AlertDialog
 import androidx.fragment.app.Fragment
 import com.mahfuznow.instagram.databinding.FragmentPostBinding
@@ -15,11 +13,6 @@ import com.mahfuznow.instagram.databinding.FragmentPostBinding
 
 class PostFragment : Fragment() {
     private lateinit var binding: FragmentPostBinding
-
-    companion object {
-        private const val CAMERA_REQUEST_CODE = 1
-        private const val GALLERY_REQUEST_CODE = 2
-    }
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View {
         binding = FragmentPostBinding.inflate(inflater, container, false)
@@ -29,7 +22,6 @@ class PostFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        //when you click on the image
         binding.imageView.setOnClickListener {
             val alertDialog = AlertDialog.Builder(requireContext())
             alertDialog.setTitle("Select Action")
@@ -55,34 +47,19 @@ class PostFragment : Fragment() {
         }
     }
 
+    private val getContentLauncher = registerForActivityResult(ActivityResultContracts.GetContent()) {
+        binding.imageView.setImageURI(it)
+    }
+
     private fun gallery() {
-        val intent = Intent(Intent.ACTION_PICK)
-        intent.type = "image/*"
-        startActivityForResult(intent, GALLERY_REQUEST_CODE)
+        getContentLauncher.launch("image/*")
+    }
+
+    private val takePicturePreviewLauncher = registerForActivityResult(ActivityResultContracts.TakePicturePreview()) {
+        binding.imageView.setImageBitmap(it)
     }
 
     private fun camera() {
-        val intent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
-        startActivityForResult(intent, CAMERA_REQUEST_CODE)
+        takePicturePreviewLauncher.launch()
     }
-
-
-    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
-        super.onActivityResult(requestCode, resultCode, data)
-        if (resultCode == Activity.RESULT_OK) {
-            when (requestCode) {
-                CAMERA_REQUEST_CODE -> {
-                    val bitmap = data?.extras?.get("data") as Bitmap
-                    binding.imageView.setImageBitmap(bitmap)
-                }
-
-                GALLERY_REQUEST_CODE -> {
-                    val uri = data?.data
-                    binding.imageView.setImageURI(uri)
-
-                }
-            }
-        }
-    }
-
 }
