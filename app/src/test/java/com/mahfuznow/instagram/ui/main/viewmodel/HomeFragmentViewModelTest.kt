17a28b0256440e1c31cbcd2f6092477fe9f26504
Md. Mahfuzur Rahman <mahfuznow@gmail.com>
2022-06-03 16:36:23 +0600

    Fixed issue: Test failed while running multiple test cases

--- app/src/test/java/com/mahfuznow/instagram/ui/main/viewmodel/HomeFragmentViewModelTest.kt
+++ app/src/test/java/com/mahfuznow/instagram/ui/main/viewmodel/HomeFragmentViewModelTest.kt
@@ -8,14 +8,14 @@ import kotlinx.coroutines.*
 import kotlinx.coroutines.test.resetMain
 import kotlinx.coroutines.test.runTest
 import kotlinx.coroutines.test.setMain
-import org.junit.After
-import org.junit.Before
-import org.junit.Rule
-import org.junit.Test
+import org.junit.*
 import org.junit.rules.TestRule
+import org.junit.runners.MethodSorters
+import java.util.*
 
 @DelicateCoroutinesApi
 @ExperimentalCoroutinesApi
+@FixMethodOrder(MethodSorters.NAME_ASCENDING)
 class HomeFragmentViewModelTest {
 
     //setting rules to execute each test synchronously
@@ -41,35 +41,49 @@ class HomeFragmentViewModelTest {
     }
 
     @Test
-    fun `load posts data`() {
+    fun `a) load posts data`() = runBlocking {
+        println("Started at ${Calendar.getInstance().timeInMillis}")
         var posts = homeFragmentViewModel.posts.getOrAwaitValue()
         assertThat(posts.data).isNull() //Loading State
+
+        delay(1)
+
         posts = homeFragmentViewModel.posts.getOrAwaitValue()
         assertThat(posts.data?.total).isEqualTo(873) //Success State
+        println("Ended at ${Calendar.getInstance().timeInMillis}")
     }
 
     @Test
-    fun `load users data`() {
+    fun `b) load users data`() = runBlocking {
+        println("Started at ${Calendar.getInstance().timeInMillis}")
         var users = homeFragmentViewModel.users.getOrAwaitValue()
         assertThat(users.data).isNull() //Loading State
+
+        delay(1)
+
         users = homeFragmentViewModel.users.getOrAwaitValue()
         assertThat(users.data?.total).isEqualTo(99) //Success State
+        println("Ended at ${Calendar.getInstance().timeInMillis}")
     }
 
 
     @Test
-    fun `demo coroutine 1`() {
+    fun `c) demo coroutine 1`() {
         runBlocking {
             launch(Dispatchers.Main) {  // Will be launched in the mainThreadSurrogate dispatcher
-                delay(2000)
+                println("Started at ${Calendar.getInstance().timeInMillis}")
+                delay(1)
                 assertThat(true).isTrue()
+                println("Ended at ${Calendar.getInstance().timeInMillis}")
             }
         }
     }
 
     @Test
-    fun `demo coroutine 2` () = runTest {
-        delay(2000)
+    fun `d) demo coroutine 2`() = runTest {
+        println("Started at ${Calendar.getInstance().timeInMillis}")
+        delay(1)
         assertThat(true).isTrue()
+        println("Ended at ${Calendar.getInstance().timeInMillis}")
     }
 }
